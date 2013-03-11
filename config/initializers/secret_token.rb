52a3bbb8e7e9d1175d633d01b0eaaf11099eeff0
Michael de Silva <michael@mwdesilva.com>
2013-03-11 13:42:32 +0530

    Perform `ruby rails_4.0.0.beta1_upgrade.rb` to upgrade app

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -2,6 +2,11 @@
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-NewApp::Application.config.secret_token = '8f8d8732afaf91077fc9d5468414f8afc460e43c0fc7be1d33cbd8d885a5015523a531b38949c8e364e3ed7954ef26b695d98488cd48833a69594a516214f4c6'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NewApp::Application.config.secret_key_base = 'a88611b3c01359ef844012956c2a0c3129b9c965ca775753b543b20b0194f8ad146e334b36e7be9a67e4cb66ef52d1a7a8f4dcf7ea7a3874a6cde42e03ee5b17'
